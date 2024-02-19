384d65aa5a9c4dc5f1c6f2dea9011841f79d2141
Erin Lipman <erlipman81@gmail.com>
2024-02-19 14:14:05 -0800

    scripts for figures

---
+++ make_data_figures.R
@@ -0,0 +1,157 @@
+library(tidyverse)
+library(yaml)
+library(ggcorrplot)
+
+configs <- read_yaml(file.path("IdealPointsCompare",
+                               "configs.yml"))
+
+in_path <- file.path("IdealPointsCompare")
+out_path <- file.path("Figures")
+dir.create(out_path, showWarnings = F)
+dir.create(file.path(out_path,"Data"), showWarnings = F)
+
+# helper functions
+make_shading <- function(dat_summary){
+  dat_summary %>% 
+    select(cong, party=party_control) %>%
+    mutate(type = 
+             case_when(party!=lag(party) | is.na(lag(party)) ~ "start", 
+                       party!=lead(party) | is.na(lead(party)) ~ "end")) %>%
+    filter(!is.na(type)) %>%
+    group_by(type) %>%
+    mutate(run=row_number()) %>%
+    ungroup() %>%
+    pivot_wider(names_from=type, values_from=cong) %>%
+    mutate(end=ifelse(is.na(end), start, end)) %>%
+    filter(party=="R") 
+}
+
+standardize <- function(col){
+  if (all(col %in% 0:1)){
+    return(col-mean(col))
+  } else {
+    return( (col-mean(col))/sd(col) )
+  }
+}
+
+percent_with_party <- function(dat){
+  y <- dat$y  
+  party <- dat$members$party
+  gamma <- c(rep(0, dat$gam0_maxC+1), rep(1,ncol(y)-dat$gam0_maxC-1))
+  R_vote <- as.numeric(colMeans(y[party=="R",], na.rm=T)>.5)
+  D_vote <- as.numeric(colMeans(y[party=="D",], na.rm=T)>.5)
+  
+  R_with <- y==matrix(rep(R_vote, nrow(y)), nrow=nrow(y), byrow = T)
+  D_with <- y==matrix(rep(D_vote, nrow(y)), nrow=nrow(y), byrow = T)
+  
+  R_with_type <- map(0:1, ~rowMeans(R_with[,gamma==.x], na.rm=T))
+  D_with_type <- map(0:1, ~rowMeans(D_with[,gamma==.x], na.rm=T))
+  
+  tibble(party=party, 
+         proc_with_party=ifelse(party=="R",R_with_type[[1]],D_with_type[[1]]),
+         final_with_party=ifelse(party=="R",R_with_type[[2]],D_with_type[[2]])) 
+}
+
+summarise_one <- function(cong_num){
+  dat <- readRDS(file.path(in_path, "Data",
+                           paste0("inputs_H",
+                                  str_pad(cong_num, 3, pad=0),
+                                  ".RDS")))
+  
+  # covariate summaries
+  X <- dat$covariates[,configs$covariates]
+  X <- mutate_all(as.data.frame(X), standardize) %>% as.matrix() 
+  
+  PC_full <- princomp(X)
+  PC <- tibble(num=1:ncol(X),
+               percent=cumsum(PC_full$sdev^2 / sum(PC_full$sdev^2)))
+  
+  prv <- dat$covariates$p_R +.5
+  maj_party <- dat$cong_info$partyControl
+  min_party <- setdiff(c("D","R"), maj_party)
+  party <- ifelse(dat$covariates$belto.partyControl, maj_party, min_party)
+                  
+  
+  tibble(cong = cong_num,
+         members = nrow(dat$y),
+         votes_proc = dat$gam0_maxC+1,
+         votes_final = ncol(dat$y) - votes_proc,
+         cond_num = kappa(X, exact = T),
+         PC = list(PC),
+         cor = list(cor(X)),
+         prv=list(tibble(party=party, prv=prv)),
+         with_party = list(percent_with_party(dat)),
+         party_control = maj_party)
+}
+
+# load data
+dat_summary <- map_df(93:113, summarise_one)
+
+shading <- make_shading(dat_summary)
+
+############################################
+
+# FIGURES FOR PAPER
+
+# Figure 1: correlation matrices
+cov_cat <- tibble(type=names(configs$covariate_cats),
+                  cov=configs$covariate_cats) %>%
+  unnest(cov)
+breaks <- cumsum(count(cov_cat,type)$n[3:2])+0.5
+
+for (CONG in c(93,98,103,108)){
+  cor <- filter(dat_summary, cong==CONG)[["cor"]][[1]]
+  cor <- cor[cov_cat$cov, cov_cat$cov]
+  g <- ggcorrplot(cor) +
+    geom_hline(yintercept=breaks, alpha=.5) +
+    geom_vline(xintercept=breaks, alpha=.5) +
+    ggtitle(paste0("H", str_pad(CONG, 3, pad="0"))) +
+    xlab("") + ylab("") +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle=45, hjust=1))
+  print(g)
+  
+  # FIGURE 1
+  ggsave(file.path(out_path, "Data",
+                   paste0("H", str_pad(CONG, 3, pad="0"), "_cor.jpeg")),
+         height = 6, width=6.5)
+}
+
+# FIGURE 2: percent voting with party
+dat_with_party <- dat_summary %>%
+  select(cong, with_party) %>%
+  unnest(with_party) %>%
+  pivot_longer(ends_with("with_party"),
+               names_to="type", values_to="percent") %>%
+  group_by(cong, type, party) %>%
+  summarise(percent=median(percent)) %>%
+  mutate(type=ifelse(grepl("final",type), "Final","Procedural"))
+
+dat_with_party %>%
+  ggplot(aes(x=cong, y=percent, color=party, linetype=type)) +
+  geom_rect(data=shading, inherit.aes = F, alpha=.2,
+            aes(ymin=min(dat_with_party$percent), 
+                ymax=1, 
+                xmin=start-.5, xmax=end+.5)) +
+  geom_line() +
+  xlab("House") +
+  scale_y_continuous("Percent voting with party") +
+  scale_color_manual("Party", values=c("blue","red")) +
+  scale_linetype_discrete("Vote type") +
+  theme_bw()
+
+ggsave(file.path(out_path, "Data","data_with_party.jpeg"),
+       width = 6, height=2.5)
+
+# FIGURE 3: republican voteshare
+dat_summary %>%
+  select(cong, prv) %>%
+  unnest(prv) %>%
+  ggplot(aes(x=as.factor(cong), y=prv, color=party)) +
+  geom_boxplot(outlier.size = .5) +
+  xlab("House") +
+  scale_y_continuous("Republican voteshare", limits = c(0,1)) +
+  scale_color_manual("Party", values=c("blue","red")) +
+  theme_bw()
+ggsave(file.path(out_path, "Data", "data_prv.jpeg"),
+       width=6, height=2.5)
---
+++ make_results_figures.R
@@ -0,0 +1,292 @@
+library(tidyverse)
+library(yaml)
+library(scales)
+library(pander)
+
+configs <- read_yaml(file.path("IdealPointsCompare","configs.yml"))
+
+args = commandArgs(trailingOnly = T)
+args <- as.list(args)
+names(args) <- c("test")
+test=(args$test==1)
+
+FLDR = "joint"
+test=T
+if (test){
+  CONGS=93:94
+} else {
+  CONGS=93:113
+}
+
+in_path <- file.path("IdealPointsCompare")
+out_path <- file.path("Figures")
+dir.create(out_path, showWarnings = F)
+
+read_one <- function(cong_num, folder,
+                     suffix="10000_5000_10"){
+  
+  suffix <- case_when(test~"10_10_1",
+                      cong_num==105~"20000_20000_25", 
+                      T~"20000_15000_20")
+  
+  cong_str = paste0("H", str_pad(cong_num, 3, pad=0))
+  
+  # get res stage 1
+  path1 = file.path(in_path,"Results",folder,"Combined",
+                    paste0(cong_str, "_", suffix, "_combined.RDS"))
+  res <- readRDS(path1)
+  
+  # data quantities
+  dat <- readRDS(file.path(in_path,"Data",
+                           paste0("inputs_", cong_str, ".RDS")))
+  
+  tht <- reduce(
+    map(res$stage2[names(res$stage2)!="covariates"], ~(.x$theta)), rbind)
+  covs <- tibble(p_R=dat$covariates$p_R,
+                 recentArrivalPrcnt=dat$covariates$recentArrivalPrcnt,
+                 dwnom1=dat$covariates$dwnom1)
+  
+  # for scaling coefficients
+  binary <- map_dbl(dat$covariates[,configs$covariates],
+                    ~all(.x %in% 0:1))
+  cov_sd <- map_dbl(dat$covariates[,configs$covariates], sd)
+  scale <- ifelse(binary, 1, cov_sd)
+  
+  tibble(summy=list(res$summy), 
+         stage2=list(res$stage2),
+         #polar=list(polar),
+         covs = list(covs),
+         party = dat$cong_info$partyControl,
+         cov_scale=list(scale),
+         res=list(res$res))
+}
+
+
+###########################################
+
+res_all <- 
+  tibble(cong=CONGS) %>%
+  mutate(res=map(cong, read_one, folder=FLDR)) %>%
+  unnest(c(res))
+
+summy_all <- select(res_all, cong, summy) %>% unnest(summy)
+
+get_model_one <- function(res2, cov_scale){
+  covariates <- res2$covariates
+  res2 <- res2[names(res2)!="covariates"]
+  eps <- map_dfr(res2, ~tibble(.$eps))[[1]]
+  eta <- map_dfr(res2, ~tibble(.$eta))[[1]]
+  if (ncol(eps)==length(covariates)){
+    eps <- cbind(1,eps)
+  }
+  
+  tibble(cov=c("int",covariates),
+         eps=colMeans(eps),
+         scale = c(1, cov_scale),
+         eta_L95=apply(eta,2,quantile,.025),
+         eta_U95=apply(eta,2,quantile,.975),
+         eta=apply(eta,2,quantile,.5)) %>%
+    filter(cov!="int")
+}
+
+get_model_size <- function(res2){
+  size <- map_dfr(res2[names(res2)!="covariates"], ~tibble(.$eps)) %>%
+    rowSums()
+  tibble(size=size-1) %>%
+    summarise(p=median(size), 
+              L95 = quantile(size,.025),
+              U95 = quantile(size, .975))
+}
+
+model_all <- select(res_all, cong,
+                    stage2, cov_scale) %>%
+  mutate(model = map2(stage2, cov_scale, get_model_one)) %>%
+  select(-stage2, -cov_scale) %>%
+  unnest(model) %>%
+  group_by(cov) %>%
+  mutate(sign = case_when(eps<.5~"",
+                          eta<0~"-", eta>0~"+", eta==0~"o"),
+         n_selected = sum(eps>.5),
+         avg_post = mean(eps),
+         max_post = max(eps)) %>%
+  ungroup()
+
+size_all <- mutate(res_all,
+                   size=map(stage2, get_model_size)) %>%
+  select(cong, size) %>%
+  unnest(size)
+
+party_info <- readRDS(file.path(in_path,"Data","party_info.RDS"))
+
+
+###########################################
+#                 ASF                     #
+###########################################
+
+# FIGURE 4
+summy_all %>% 
+  filter(type=="All") %>%
+  ggplot(aes(x=cong, y=ASF, ymin=ASF_L95, ymax=ASF_U95)) +
+  geom_linerange(position = position_dodge(.5)) +
+  geom_point(position = position_dodge(.5), size=1) +
+  geom_rect(data=party_info, ymin=0, ymax=1,
+            aes(xmin=start-.5, xmax=end+.5),
+            inherit.aes = F, alpha=.2) +
+  xlab("House") + ylab("Bridging frequency") +
+  theme_bw()
+
+ggsave(file.path(out_path,"ASF.jpeg"), height = 2.5, width=6)
+
+# FIGURE 8
+summy_all %>% 
+  ggplot(aes(x=cong, y=ASF,  ymin=ASF_L95,  ymax=ASF_U95, 
+             color=type, linetype=type)) +
+  geom_linerange(position = position_dodge(.5)) +
+  geom_point(position = position_dodge(.5), size=1) +
+  geom_rect(data=party_info, ymin=0, ymax=1,
+            aes(xmin=start-.5, xmax=end+.5),
+            inherit.aes = F, alpha=.2) +
+  xlab("House") + ylab("Bridging frequency") +
+  scale_color_manual("Party",values=c("black","blue","red")) +
+  scale_linetype_manual("Party",values=c("solid","dashed","dashed")) +
+  theme_bw()
+
+ggsave(file.path(out_path,"ASF_party.jpeg"), height = 2.5, width=6)
+
+###########################################
+#              Model size                 #
+###########################################
+
+# FIGURE 5
+size_all %>%
+  ggplot(aes(x=cong, y=p, ymax=L95, ymin=U95)) +
+  geom_rect(data=party_info, ymin=0, ymax=21,
+            aes(xmin=start-.5, xmax=end+.5),
+            inherit.aes = F, alpha=.2) +
+  geom_linerange(position=position_dodge(.5)) +
+  geom_point(position=position_dodge(.5), size=1) +
+  xlab("Congress") +
+  ylab("Median model size") +
+  theme_bw()
+
+model_all %>%
+  group_by(cong) %>%
+  summarise(p=sum(eps>.5)) %>% 
+  ggplot(aes(x=cong, y=p)) +
+  geom_rect(data=party_info, ymin=0, ymax=21,
+            aes(xmin=start-.5, xmax=end+.5),
+            inherit.aes = F, alpha=.2) +
+  #geom_linerange(position=position_dodge(.5)) +
+  geom_point(position=position_dodge(.5), size=1) +
+  xlab("House") +
+  ylab("Median model size") +
+  theme_bw()
+
+ggsave(file.path(out_path,"size.jpeg"),
+       height = 2.5, width=6)
+
+###########################################
+#            SELECTED MODELS              #
+###########################################
+
+# FIGURE 6
+
+cov_cat <- tibble(type=names(configs$covariate_cats),
+                  cov=configs$covariate_cats) %>%
+  unnest(cov)
+
+dat_colorplot <- model_all %>%
+  select(cong,cov,eps,n_selected,sign) %>%
+  full_join(cov_cat, by="cov") %>%
+  arrange(type,n_selected) %>%
+  mutate(cov=fct_relevel(cov,unique(cov)))
+
+dat_colorplot %>% 
+  rename(PIP=eps) %>%
+  ggplot(aes(x=cong, y=cov, fill=PIP)) +
+  geom_tile() +
+  #geom_point(data=filter(dat_colorplot,eps>.5)) +
+  geom_text(aes(label=sign), size=5) +
+  geom_hline(yintercept=nrow(cov_cat)-cumsum(count(cov_cat,type)$n[3:1])+0.5,
+             color="white") +
+  scale_fill_gradientn(colors=c("blue","white","red"), 
+                       breaks=c(0,.5,1)) +
+  xlab("House") + ylab("") +
+  theme_minimal()
+ggsave(file.path(out_path,"red_blue.jpeg"), height = 4, width=6)
+
+###########################################
+#              COEFFICIENTS               #
+###########################################
+
+# FIGURE 7
+
+plot_cov <- function(which_cov,title="", c=1,
+                     lab_pow=10){
+  
+  plot_dat <- model_all %>%
+    filter(cov==which_cov) %>%
+    mutate_at(vars(contains("eta")), ~exp(.x*(c/scale)))
+  
+  print(paste0("Showing ORs for an increase of (median) ", 
+               round(c/median(plot_dat$scale),2),
+               " standard deviations"))
+  
+  shading <- 
+    plot_dat %>% 
+    #group_by(type) %>%
+    summarize(min=min(c(eta,eta_L95)), max=max(eta,eta_U95)) %>%
+    mutate(party_info = list(party_info)) %>%
+    unnest(party_info)
+  
+  lrange = round(log(c(min(shading$min), max(shading$max)),
+                     lab_pow))
+  lrange =seq(lrange[1],lrange[2])
+  labels = ifelse(lrange>=0,
+                  round(lab_pow^lrange),lab_pow^lrange)
+  
+  g <- plot_dat %>% 
+    ggplot(aes(x=eta, y=cong, xmin=eta_L95,xmax=eta_U95)) +
+    geom_rect(data=shading, inherit.aes = F, alpha=.2,
+              aes(xmin=min, xmax=max, ymin=start-.5, ymax=end+.5)) +
+    #geom_vline(data=refline, inherit.aes=F,
+    #           aes(xintercept=ref), linetype="dotted") +
+    geom_vline(xintercept=1, linetype="dotted") +
+    geom_pointrange() +
+    scale_x_continuous(trans = log_trans(),
+                       labels = labels,
+                       breaks = lab_pow^lrange) +
+    #scale_x_log10() +
+    #facet_wrap(type~., ncol=1, scales="free_y") +
+    coord_flip() +
+    xlab("Increase in odds ratio\n(log scale)") + ylab("House") +
+    ggtitle(title) +
+    theme_bw()
+  plot(g)
+  
+  num_summy <- select(plot_dat, cong, eps, OR=eta) %>%
+    filter(eps>.5)%>%
+    mutate(eta=log(OR), OR_inv=1/OR) %>%
+    arrange(OR)
+  print(num_summy)
+  
+  ggsave(file.path(out_path, paste0(which_cov,".jpeg")), 
+         height = 2.5, width=6)
+}
+
+select(model_all, cov, n_selected) %>%
+  distinct() %>%
+  arrange(-n_selected) %>%
+  filter(n_selected>3) 
+
+#paste0("Belongs to majority party",
+#        "\nOR for majority vs. minority")
+plot_cov("belto.partyControl", c=1)
+
+#paste0("Constituancy (Democratic legislators only)",
+#"\nOR for increase of 5 percent")
+plot_cov("p_R_D", c=.05, lab_pow = 2)
+
+#paste0("Constituancy (Republican legislators only)",
+#"\nOR for increase of 5 percent")
+plot_cov("p_R_R", c=.05, lab_pow = 2)
